cf25a82b66dfb894f516b3d998a3ee22613da12e
ktanshad <anshadkt212@gmail.com>
2023-12-02 17:11:28 +0530

    changed http to dio

--- lib/controller/userlist_provider.dart
+++ lib/controller/userlist_provider.dart
@@ -1,11 +1,10 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:studentlistapi/model/userlist_model.dart';
+import 'package:studentlistapi/services/userlist_provider.dart';
 
 class UserListProvider extends ChangeNotifier{
   List<UserModel> studentList=[];
+  UserListServices userListServices=UserListServices();
 
 
   UserListProvider(){
@@ -13,9 +12,7 @@ class UserListProvider extends ChangeNotifier{
   }
 
   Future getData()async{
-    String url='https://jsonplaceholder.typicode.com/users';
-    var response=await http.get(Uri.parse(url));
-    var jsondecode=jsonDecode(response.body);
+     final jsondecode=await userListServices.getData();
     studentList=(jsondecode as List).map((e){
 
       return UserModel.fromJson(e as Map<String,dynamic>);
---
+++ lib/services/userlist_provider.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+
+class UserListServices{
+Dio _dio=Dio();
+
+    Future<dynamic> getData()async{
+    String url='https://jsonplaceholder.typicode.com/users';
+    var response=await _dio.get(url);
+    var jsondecode=response.data;
+    return jsondecode;
+  }
+}
